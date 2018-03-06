e0e21c9cae027e3ef6f69339fdd876b69ddcead2
dcatfly <772736731@qq.com>
2018-03-07 00:11:40 +0800

    add upload & refresh

--- README.md
+++ README.md
@@ -2,17 +2,56 @@
 > 将webpack编译之后的文件上传到七牛云
 
 ## 特性
--[ ] 支持上传到七牛云指定`bucket`
--[ ] 支持排除指定文件
--[ ] 支持对比上传
--[ ] 支持上传前清空指定`bucket`
--[ ] 支持上传完成后刷新cdn
+- [x] 支持上传到七牛云指定`bucket`
+- [x] 支持排除指定文件
+- [ ] 支持对比上传
+- [ ] 支持上传前清空指定`bucket`
+- [x] 支持上传完成后刷新cdn
 
 ## 安装
 
 ## 使用方法
+```js
+//webpack config
+const Qiniu = require('webpack-qiniu-plugin')
 
-## 致谢
+const CDN_HOST = `https://static.qiniuxxx.com/`
+module.exports = {
+  entry: 'app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'app.[chunkhash].js',
+    //配置webpack打包后插入文件时的cdn
+    publicPath: CDN_HOST
+  },
+  plugins: [
+    new Qiniu({
+        accessKey: 'accessKey',
+        secretKey: 'secretKey',
+        bucket: 'static_bucket',
+        zone: 'Zone_z0',
+        exclude: /\.html/,
+        refreshCDN: CDN_HOST
+    })
+  ]
+}
+```
+## 参数
+|Name|Type|Default|Description|
+|:--:|:--:|:-----:|:----------|
+|**[`accessKey`](#)**|`{Sring}`||七牛提供的accessKey|
+|**[`secretKey`](#)**|`{Sring}`||七牛提供的secretKey|
+|**[`bucket`](#)**|`{Sring}`||七牛云存储中的bucket|
+|**[`zone`](#)**|`{Sring}`|`Zone_z1`|七牛云存储位置，华东 `Zone_z0`、华北 `Zone_z1`、华南 `Zone_z2`、北美 `Zone_na0`|
+|**[`chunkSize`](#)**|`{Number}`|`20`|每次并行上传的文件个数|
+|**[`exclude`](#)**|`{Regexp}`||要排除的文件名正则规则|
+|**[`refreshCDN`](#)**|`{Sring}`||想要刷新cdn的域名，不填写默认不刷新cdn，填写默认上传完成后刷新此次上传的所有文件|
 
 ## License
+
+```
+MIT License
+
+Copyright (c) 2018 ZhangZhiheng
+```
 
--- index.js
+++ index.js
@@ -1,11 +1,73 @@
 const qiniu = require('qiniu')
 const ora = require('ora');
+const chunk = require('lodash.chunk')
+const path = require('path')
+
+//七牛配置初始化
+const config = new qiniu.conf.Config();
+
+//七牛刷新cdn每次最大的数目
+const MAX_REFRESH_CDN = 100
 
 // 上传进度
-const tip = (uploaded, total) => {
-  let percentage = Math.round(uploaded / total * 100);
-  return `Uploading to Qiniu CDN: ${percentage}% ${uploaded}/${total} files uploaded`;
+const spinner = ora({
+  color: 'green'
+});
+const tip = ({done = 0, total, type = 'uploading'}) => {
+  if(total){
+    let percentage = Math.round(done / total * 100);
+    spinner.text = `Qiniu CDN ${type}: ${percentage}% ${done}/${total} `;
+  }else{
+    spinner.text = 'wait for webpack compiler...'
+  }
+
+  if (done === 0){
+    console.log('\n');
+    spinner.start()
+  }
+  done === total && spinner.succeed()
+
 };
+
+
+
+//上传完成
+const finish = ({err, callback}) => {
+  err && spinner.fail()
+  callback && callback(err)
+}
+
+//刷新CDN 限额500每天
+const refresh = (fileNames, mac, cdn) => {
+  //@TODO 增加处理函数 可以指定刷新符合处理函数规则的file
+  const cdnManager = new qiniu.cdn.CdnManager(mac);
+  let total = fileNames.length, refreshed = 0, type = 'refreshing';
+  tip({type})
+  const _refresh = (urls) => {
+    return new Promise((resolve, reject) => {
+      cdnManager.refreshUrls(urls, function (err, respBody, respInfo) {
+        refreshed += urls.length;
+        tip({done: refreshed, total, type})
+        if (err) {
+          console.log(respBody)
+          reject(err)
+        }
+        if (respInfo.statusCode == 200) {
+          resolve()
+        }else {
+          console.log(respInfo)
+          reject(respBody)
+        }
+      });
+    })
+  }
+  //每次只能包含100个cdn链接
+  return Promise.all(chunk(fileNames, MAX_REFRESH_CDN).
+      map(chunkFiles => _refresh(chunkFiles.map(name => path.join(cdn, name))))
+  )
+
+}
+
 
 
 module.exports = class QiniuPlugin {
@@ -19,23 +81,20 @@ module.exports = class QiniuPlugin {
   apply(compiler) {
     compiler.plugin('after-emit', (compilation, callback) => {
       const {assets} = compilation;
+      const {bucket, zone, accessKey, secretKey, chunkSize = 20, exclude, clear, refreshCDN} = this.options;
+
       const fileNames = Object.keys(assets).filter((fileName) => {
         const file = assets[fileName] || {};
-        if (!file.emitted) return false;
+        if (!file.emitted || new RegExp(exclude).test(fileName)) return false;
         return true
       })
-      const {bucket, zone, accessKey, secretKey, clear, refresh} = this.options;
       let total = fileNames.length, uploaded = 0;
 
-      const config = new qiniu.conf.Config();
       config.zone = qiniu.zone[zone] || qiniu.zone.Zone_z1;
       qiniu.conf.RPC_TIMEOUT = 600000;
 
-      console.log('\n');
-      let spinner = ora({
-        text: tip(0, total),
-        color: 'green'
-      }).start();
+      //打印上传状态
+      tip({})
 
       //构建上传单个文件函数
       const upload = (fileName) => {
@@ -47,9 +106,9 @@ module.exports = class QiniuPlugin {
         const putExtra = new qiniu.form_up.PutExtra();
 
         return new Promise((resolve, reject) => {
-          formUploader.putFile(upToken, fileName, file.existsAt, putExtra, function (err, respBody, respInfo) {
+          return formUploader.putFile(upToken, fileName, file.existsAt, putExtra, function (err, respBody, respInfo) {
             uploaded++;
-            spinner.text = tip(uploaded, total);
+            tip({done: uploaded, total});
             if (err) {
               console.log(fileName + ' upload failed')
               reject(err)
@@ -67,18 +126,31 @@ module.exports = class QiniuPlugin {
 
           });
         })
-
-
-
-
-
       }
 
+      //分块执行上传函数
+      const copyFileNames = [...fileNames]
+      const uploadChunk = (err) => {
+        const chunkFiles = [].splice.call(copyFileNames, 0, chunkSize)
+        if (err) return Promise.reject(err)
+        if(chunkFiles.length > 0){
+          return Promise.all(chunkFiles.map(fileName => upload(fileName))).
+              then(() => uploadChunk()).catch(uploadChunk)
+        }else{
+          return Promise.resolve()
+        }
+      }
 
-      console.log(Object.keys(assets))
+      uploadChunk().then(() => {
+        const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+        return refreshCDN && refresh(fileNames, mac, refreshCDN)
+      }).then(() => {
+        finish({callback})
+      }).catch((err) => {
+        finish({callback, err})
+      })
 
 
-      callback()
     })
   }
 }
